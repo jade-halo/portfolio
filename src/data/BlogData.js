b2ce1f357319db68f01b2089c2d73d6fc5b958c4
jade-halo <ydh99151@gmail.com>
2024-12-27 14:51:34 +0900

    241227_v2

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -4,15 +4,15 @@ export const Blogs = [
     {
         id:1,
         name:"국내 첫 프롬프트 경진대회에 비개발자가 참가한 이유",
-        tags:["뤼튼","프롬프톤", "프롬프트엔지니어링"],
+        tags:["해커톤","프롬프팅","뤼튼","프롬프톤"],
         date:"2023년 5월",
         imgSrc:"https://static.onoffmix.com/afv2/thumbnail/2023/05/19/27b8173ea518b1d9ce69c6519b7061eb.png",
-        link:"https://wrtn.io/en/2023/12/19/%EB%AA%A8%EB%91%90%EB%A5%BC-%EC%9C%84%ED%95%9C-ai-%EB%A4%BC%ED%8A%BC-%ED%94%84%EB%A1%AC%ED%94%84%ED%86%A4-%EC%83%9D%EC%84%B1-ai-%EA%B2%BD%EC%A7%84%EB%8C%80%ED%9A%8C/"
+        link:"https://sm4s5ak3k8iie6iw.vercel.app/"
     },
     {
         id:2,
         name:"생성형 AI로 2024년 목표를 다시 되새기다",
-        tags:["사내공모전", "AI이미지", "최우수상"],
+        tags:["사내공모전", "생성형AI", "최우수상","1분46초부터"],
         date:"2024년 3월",
         imgSrc:"https://img.youtube.com/vi/PVqzOuqfYFg/hqdefault.jpg",
         link:"https://www.youtube.com/watch?v=PVqzOuqfYFg"
@@ -20,7 +20,7 @@ export const Blogs = [
     {
         id:3,
         name:"주임이 모든 임직원들에게 가르쳐 줄 수 있는 것",
-        tags:["생성형AI", "ChatGPT", "사내강사"],
+        tags:["사내강사", "생성형AI", "ChatGPT"],
         date:"2024년 3월",
         imgSrc:"https://img.youtube.com/vi/9wOlTM0vLug/hqdefault.jpg",
         link:"https://www.youtube.com/playlist?list=PLdF8FeJCJHbI68b5fQnvBzJ8RPz7zqrlp"
@@ -28,15 +28,15 @@ export const Blogs = [
     {
         id:4,
         name:"AI 프로젝트로 그룹CEO 라운드 테이블에 소개된 이야기",
-        tags:["GPTs","MeetBot","회의문화개선"],
+        tags:["AI서비스","MeetBot","회의문화개선"],
         date:"2024년 9월",
         imgSrc:"https://img.youtube.com/vi/jXJ2CPMwZAM/hqdefault.jpg",
-        link:"https://youtu.be/jXJ2CPMwZAM"
+        link:"https://www.canva.com/design/DAGRLh2PpeE/wZobZEXCs-Dcvix5MfTWMg/view?utm_content=DAGRLh2PpeE&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=haa22c94ee2"
     },
     {
         id:5,
-        name:"예산이 없다구요? 그럼 제가 직접 광고만들게요",
-        tags:["AI영상","리챔","브랜드필름"],
+        name:"예산이 없다구요? 그럼 제가 AI로 만들어 볼게요",
+        tags:["생성형AI","Image-to-Video","리챔","브랜드필름"],
         date:"2024년 10월",
         imgSrc:"https://img.youtube.com/vi/pd_W4B_ROms/hqdefault.jpg",
         link:"https://youtu.be/pd_W4B_ROms"
