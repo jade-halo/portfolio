e53e8e59b82b6d6b807a5b7a896e7cf832f5d3dc
jade-halo <ydh99151@gmail.com>
2025-04-22 12:59:24 +0900

    250422

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -63,7 +63,7 @@ const SocialIcons = (props) => {
         <a
           style={{ color: "inherit" }}
           target="_blank"
-          href={"https://featpaper.com/l/fnPjOdAD"}
+          href={"https://featpaper.com/l/VXBfFXXl"}
         >
           <Link
             width={25}
